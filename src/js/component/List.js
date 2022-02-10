f7b344375e4cbd31f05c85fd73010a03aef24be1
Jarraxus <buckman.joshua@gmail.com>
2022-02-10 19:10:26 +0000

    API progress

--- src/js/component/List.js
+++ src/js/component/List.js
@@ -1,9 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Task } from "./Task";
 
 export const List = () => {
 	const [task, setTask] = useState("");
 	const [listedItems, setListedItems] = useState([]);
+	let url = "https://assets.breatheco.de/apis/fake/todos/user/Jarraxus"; // setting the API url as a variable
+
+	// running getList() on page load
+	useEffect(() => {
+		getList();
+	}, []); // <- ONLY on page load
+
+	function getList() {
+		fetch(url) // calling the URL variable
+			.then((response) => {
+				// checking if response is ok. If not, sending Error code
+				if (!response.ok) {
+					throw Error(response.statusText);
+				} // if response good, read the response
+				return response.json(); // Read the response as json.
+			})
+			.then((responseAsJson) => {
+				// Do stuff with the JSONified response
+				let result = [];
+				for (var i in responseAsJson) result.push([responseAsJson[i]]);
+				console.log("this is the response", responseAsJson);
+				console.log("this is the result", result);
+				setListedItems(result); // setting the listedItems as the responseAsJson we imported - currently broke as hell
+
+				console.log("this is the list", listedItems);
+			})
+			.catch((error) => {
+				console.log("Looks like there was a problem: \n", error);
+			});
+	}
 
 	const Add = (e) => {
 		if (e.keyCode == 13 && e.target.value != "") {
