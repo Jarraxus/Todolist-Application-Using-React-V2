e6150bfcde84f5651c4696dafb8d582fb7d0555c
Jarraxus <buckman.joshua@gmail.com>
2022-02-11 21:03:26 +0000

    All API functions working as intended

--- src/js/component/List.js
+++ src/js/component/List.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { Task } from "./Task";
 
 export const List = () => {
 	const [task, setTask] = useState("");
@@ -31,7 +30,21 @@ export const List = () => {
 			headers: {
 				"Content-Type": "application/json",
 			},
-		});
+		})
+			.then((resp) => {
+				console.log(resp.ok); // will be true if the response is successfull
+				console.log(resp.status); // the status code = 200 or code = 400 etc.
+				console.log(resp.text()); // will try return the exact result as string
+				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then((data) => {
+				//here is were your code should start after the fetch finishes
+				console.log("This is the data", data); //this will print on the console the exact object received from the server
+			})
+			.catch((error) => {
+				//error handling
+				console.log(error);
+			});
 	}
 
 	const Add = (e) => {
@@ -74,12 +87,12 @@ export const List = () => {
 				<div className="listed-items">
 					<ul>
 						{listedItems.map((item, index) => (
-							<Task
-								key={index}
-								ind={index}
-								task={item}
-								remove={Remove}
-							/>
+							<li className="listed-tasks" key={index}>
+								{item.label}
+								<i
+									className="delete fa fa-trash"
+									onClick={() => Remove(index)}></i>
+							</li>
 						))}
 					</ul>
 				</div>
